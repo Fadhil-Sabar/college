8c8c44b10f401b2644576cc4aeba4c1f839cc42c
Fadhil Andriawan <fadhil@ilcs.co.id>
2025-10-30 16:47:30 +0700

    [UPDATE] fitur stok & navbar

---
+++ sitta-praktik/js/script.js
@@ -0,0 +1,11 @@
+function openTracking() {
+    window.location.href = 'tracking.html';
+}
+
+function openStok() {
+    window.location.href = 'stok.html';
+}
+
+function openDashboard() {
+    window.location.href = 'dashboard.html';
+}
